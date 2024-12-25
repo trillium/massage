8decc3c50b85a6a3953cb7eb3cbb381d7a93f3b2
Trillium Smith <spiteless@gmail.com>
2024-12-25 15:08:44 -0800

    feat: Move over app pages files from www-massage

---
+++ app/book/page.tsx
@@ -0,0 +1,21 @@
+import type { InferGetServerSidePropsType } from 'next'
+
+import ClientPage from './ClientPage'
+import Template from '@/components/Template'
+import { fetchData } from 'lib/fetch/fetchData'
+import { applyReferral } from 'lib/posthog/applyReferral'
+
+export type PageProps = InferGetServerSidePropsType<typeof fetchData>
+
+export const dynamic = 'force-dynamic'
+
+export default async function Page({ searchParams }: { searchParams: URLSearchParams }) {
+  const { props } = await fetchData({ searchParams })
+  applyReferral({ searchParams })
+  return (
+    <main className="mx-4 max-w-2xl pb-24 sm:mx-auto">
+      <Template title="Book a session with Trillium :)" />
+      <ClientPage {...props} />
+    </main>
+  )
+}
