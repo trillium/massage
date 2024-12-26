78f9d2b5fc78681cb5d7b3325bbf8d6be890f33a
Trillium Smith <spiteless@gmail.com>
2024-12-26 11:57:07 -0800

    style: Resolve eslint warnings for DurationPicker

--- components/availability/controls/DurationPicker.tsx
+++ components/availability/controls/DurationPicker.tsx
@@ -1,8 +1,8 @@
-import clsx from "clsx"
+import clsx from 'clsx'
 
-import { ALLOWED_DURATIONS as DEFAULT_ALLOWED_DURATIONS } from "@/config"
-import { setDuration } from "@/redux/slices/availabilitySlice"
-import { useAppDispatch, useReduxAvailability } from "@/app/hooks"
+import { ALLOWED_DURATIONS as DEFAULT_ALLOWED_DURATIONS } from 'config'
+import { setDuration } from 'redux/slices/availabilitySlice'
+import { useAppDispatch, useReduxAvailability } from 'app/hooks'
 
 export type durationProps = {
   title: string
@@ -18,13 +18,12 @@ export default function DurationPicker({ title, allowedDurations }: durationProp
     dispatchRedux(setDuration(Number(event.target.value)))
   }
 
-
   return (
     <fieldset>
-      <legend className="block text-sm font-medium leading-0 text-gray-900 dark:text-gray-100">
-        {title || "Duration"}
+      <legend className="leading-0 block text-sm font-medium text-gray-900 dark:text-gray-100">
+        {title || 'Duration'}
       </legend>
-      <div className="isolate inline-flex rounded-md shadow-sm mt-1 h-9 active:ring-2 focus-within:ring-2 focus-within:ring-primary-500 active:ring-primary-500">
+      <div className="isolate mt-1 inline-flex h-9 rounded-md shadow-sm focus-within:ring-2 focus-within:ring-primary-500 active:ring-2 active:ring-primary-500">
         {ALLOWED_DURATIONS.map((theDuration, i) => (
           <div key={theDuration} className="flex items-center">
             <input
@@ -39,14 +38,14 @@ export default function DurationPicker({ title, allowedDurations }: durationProp
             <label
               htmlFor={`duration-${theDuration}`}
               className={clsx(
-                "relative inline-flex items-center px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10 outline-primary-600",
+                'relative inline-flex items-center px-3 py-2 text-sm font-semibold outline-primary-600 ring-1 ring-inset focus:z-10',
                 {
-                  "rounded-l-md": i === 0,
-                  "rounded-r-md": i === ALLOWED_DURATIONS.length - 1,
-                  "-ml-px": i > 0,
-                  "bg-white text-gray-900 ring-gray-300 hover:bg-gray-200":
+                  'rounded-l-md': i === 0,
+                  'rounded-r-md': i === ALLOWED_DURATIONS.length - 1,
+                  '-ml-px': i > 0,
+                  'bg-white text-gray-900 ring-gray-300 hover:bg-gray-200':
                     theDuration !== duration,
-                  "bg-primary-500 text-white ring-primary-400 shadow-inner shadow-primary-900":
+                  'bg-primary-500 text-white shadow-inner shadow-primary-900 ring-primary-400':
                     theDuration === duration,
                 }
               )}
@@ -56,6 +55,6 @@ export default function DurationPicker({ title, allowedDurations }: durationProp
           </div>
         ))}
       </div>
-    </fieldset >
+    </fieldset>
   )
 }
