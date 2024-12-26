9042f9b65099c416536570f47653c76d92addc24
Trillium Smith <spiteless@gmail.com>
2024-12-25 17:13:48 -0800

    style: Resolve eslint warnings for /admin

--- app/admin/ClientPage.tsx
+++ app/admin/ClientPage.tsx
@@ -1,7 +1,7 @@
-"use client"
+'use client'
 
-import URIMaker from "@/components/URIMaker"
-import type { GoogleCalendarV3Event } from "@/lib/types"
+import URIMaker from '@/components/URIMaker'
+import type { GoogleCalendarV3Event } from 'lib/types'
 
 export default function ClientPage({ events }: { events: GoogleCalendarV3Event[] }) {
   return (
