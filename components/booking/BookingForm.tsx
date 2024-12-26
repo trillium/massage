f2a6f78f16262de5781c3090f79c24626e0d02f0
Trillium Smith <spiteless@gmail.com>
2024-12-26 12:05:20 -0800

    fix: Update autoComplete values to 'given-name' 'family-name' in BookingForm

--- components/booking/BookingForm.tsx
+++ components/booking/BookingForm.tsx
@@ -1,29 +1,29 @@
-"use client"
+'use client'
 
-import React from "react"
-import { Dialog } from "@headlessui/react"
-import { useRouter } from "next/navigation"
-import type { FormEvent } from "react"
+import React from 'react'
+import { Dialog } from '@headlessui/react'
+import { useRouter } from 'next/navigation'
+import type { FormEvent } from 'react'
 
-import Modal from "@/components/Modal"
-import Spinner from "@/components/Spinner"
-import { formatLocalDate, formatLocalTime } from "@/lib/availability/helpers"
+import Modal from 'components/Modal'
+import Spinner from 'components/Spinner'
+import { formatLocalDate, formatLocalTime } from 'lib/availability/helpers'
 
-import { setForm } from "@/redux/slices/formSlice"
-import type { AppDispatch } from "@/redux/store"
-import { DEFAULT_PRICING } from "@/config"
-import { setModal } from "@/redux/slices/modalSlice"
+import { setForm } from 'redux/slices/formSlice'
+import type { AppDispatch } from 'redux/store'
+import { DEFAULT_PRICING } from 'config'
+import { setModal } from 'redux/slices/modalSlice'
 import {
   useAppDispatch,
   useReduxAvailability,
   useReduxFormData,
   useReduxEventContainers,
   useReduxModal,
-} from "@/app/hooks"
-import { ChairAppointmentBlockProps, PaymentMethodType } from "@/lib/types"
-import { paymentMethod } from "@/data/paymentMethods"
-import siteMetadata from "@/data/siteMetadata"
-import clsx from "clsx"
+} from 'app/hooks'
+import { ChairAppointmentBlockProps, PaymentMethodType } from 'lib/types'
+import { paymentMethod } from 'data/paymentMethods'
+import siteMetadata from 'data/siteMetadata'
+import clsx from 'clsx'
 const { eventBaseString } = siteMetadata
 
 /**
@@ -51,13 +51,17 @@ type BookingFormProps = {
   endPoint: string
 }
 
-export default function BookingForm({ additionalData = {}, endPoint, acceptingPayment=true }: BookingFormProps) {
+export default function BookingForm({
+  additionalData = {},
+  endPoint,
+  acceptingPayment = true,
+}: BookingFormProps) {
   const dispatchRedux = useAppDispatch()
   const formData = useReduxFormData()
   const eventContainers = useReduxEventContainers()
   const { status: modal } = useReduxModal()
   const { selectedTime, timeZone, duration } = useReduxAvailability()
-  const price = duration ? DEFAULT_PRICING[duration] : "null"
+  const price = duration ? DEFAULT_PRICING[duration] : 'null'
 
   const router = useRouter()
 
@@ -69,7 +73,7 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
   const startString = formatLocalTime(selectedTime.start, { timeZone })
   const endString = formatLocalTime(selectedTime.end, {
     timeZone,
-    timeZoneName: "shortGeneric",
+    timeZoneName: 'shortGeneric',
   })
 
   const formOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -79,12 +83,13 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
 
   return (
     <Modal
-      open={modal !== "closed"}
+      open={modal !== 'closed'}
       setOpen={(open) => {
-        dispatchRedux(setModal({ status: open ? "open" : "closed" }))
-      }}>
+        dispatchRedux(setModal({ status: open ? 'open' : 'closed' }))
+      }}
+    >
       <form
-        className="mt-3 sm:mt-0 sm:ml-4"
+        className="mt-3 sm:ml-4 sm:mt-0"
         onSubmit={(event) => {
           handleSubmit({
             event,
@@ -93,10 +98,12 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
             additionalData,
             endPoint,
           })
-        }}>
+        }}
+      >
         <Dialog.Title
           as="h3"
-          className="text-base font-semibold leading-6 text-gray-900 dark:text-gray-100">
+          className="text-base font-semibold leading-6 text-gray-900 dark:text-gray-100"
+        >
           Request appointment
         </Dialog.Title>
 
@@ -105,12 +112,7 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
         <input type="hidden" readOnly name="duration" value={duration || 0} />
         <input type="hidden" readOnly name="price" value={price} />
         <input type="hidden" readOnly name="timeZone" value={timeZone} />
-        <input
-          type="hidden"
-          readOnly
-          name="eventBaseString"
-          value={eventBaseString}
-        />
+        <input type="hidden" readOnly name="eventBaseString" value={eventBaseString} />
         {eventContainers && eventContainers.eventBaseString && (
           <input
             type="hidden"
@@ -128,8 +130,8 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
           />
         )}
 
-        <div className="border-l-4 border-l-primary-400 bg-primary-50/30 dark:bg-primary-50/10 p-3 mt-3 mb-4 rounded-md">
-          <p className="text-sm md:text-base font-semibold text-primary-800 dark:text-primary-400">
+        <div className="mb-4 mt-3 rounded-md border-l-4 border-l-primary-400 bg-primary-50/30 p-3 dark:bg-primary-50/10">
+          <p className="text-sm font-semibold text-primary-800 dark:text-primary-400 md:text-base">
             {dateString}
           </p>
           <p className="text-xs md:text-sm">
@@ -139,18 +141,19 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
 
         <div className="flex flex-col space-y-4">
           <div className="isolate -space-y-px rounded-md shadow-sm">
-            <div className="last:rounded-t-none first:rounded-b-none last:rounded-md first:rounded-md relative px-3 pt-2.5 pb-1.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-primary-400 flex row">
-              <div className="w-full mx-1">
+            <div className="row relative flex px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-gray-300 first:rounded-md first:rounded-b-none last:rounded-md last:rounded-t-none focus-within:z-10 focus-within:ring-2 focus-within:ring-primary-400">
+              <div className="mx-1 w-full">
                 <label
                   htmlFor="name"
-                  className="block text-sm font-medium text-gray-900 dark:text-gray-100">
+                  className="block text-sm font-medium text-gray-900 dark:text-gray-100"
+                >
                   First Name
                 </label>
                 <input
                   aria-label="Name"
                   type="text"
                   autoCapitalize="words"
-                  autoComplete="firstName"
+                  autoComplete="given-name"
                   required
                   aria-required
                   name="firstName"
@@ -158,20 +161,21 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
                   value={formData.firstName}
                   placeholder="First"
                   onChange={formOnChange}
-                  className="pl-2 py-1 block w-full border-0 p-0 text-gray-900 dark:text-gray-100 placeholder:text-gray-400 focus:ring-0 sm:text-base sm:leading-6 mb-1"
+                  className="mb-1 block w-full border-0 p-0 py-1 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 dark:text-gray-100 sm:text-base sm:leading-6"
                 />
               </div>
-              <div className="w-full mx-1">
+              <div className="mx-1 w-full">
                 <label
                   htmlFor="name"
-                  className="block text-sm font-medium text-gray-900 dark:text-gray-100">
+                  className="block text-sm font-medium text-gray-900 dark:text-gray-100"
+                >
                   Last Name
                 </label>
                 <input
                   aria-label="Name"
                   type="text"
                   autoCapitalize="words"
-                  autoComplete="lastName"
+                  autoComplete="family-name"
                   required
                   aria-required
                   name="lastName"
@@ -179,14 +183,15 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
                   value={formData.lastName}
                   placeholder="Last"
                   onChange={formOnChange}
-                  className="pl-2 py-1 block w-full border-0 p-0 text-gray-900 dark:text-gray-100 placeholder:text-gray-400 focus:ring-0 sm:text-base sm:leading-6 mb-1"
+                  className="mb-1 block w-full border-0 p-0 py-1 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 dark:text-gray-100 sm:text-base sm:leading-6"
                 />
               </div>
             </div>
-            <div className="last:rounded-t-none first:rounded-b-none last:rounded-md first:rounded-md relative px-3 pt-2.5 pb-1.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-primary-400">
+            <div className="relative px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-gray-300 first:rounded-md first:rounded-b-none last:rounded-md last:rounded-t-none focus-within:z-10 focus-within:ring-2 focus-within:ring-primary-400">
               <label
                 htmlFor="phone"
-                className="block text-xs font-medium text-gray-900 dark:text-gray-100">
+                className="block text-xs font-medium text-gray-900 dark:text-gray-100"
+              >
                 Phone Number
               </label>
               <input
@@ -197,15 +202,16 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
                 name="phone"
                 id="phone"
                 value={formData && formData.phone}
-                className="pl-2 py-1 block w-full border-0 p-0 text-gray-900 dark:text-gray-100 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 mb-1"
+                className="mb-1 block w-full border-0 p-0 py-1 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 dark:text-gray-100 sm:text-sm sm:leading-6"
                 placeholder="(555) 444 - 3333"
                 onChange={formOnChange}
               />
             </div>
-            <div className="last:rounded-t-none first:rounded-b-none last:rounded-md first:rounded-md relative px-3 pt-2.5 pb-1.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-primary-400">
+            <div className="relative px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-gray-300 first:rounded-md first:rounded-b-none last:rounded-md last:rounded-t-none focus-within:z-10 focus-within:ring-2 focus-within:ring-primary-400">
               <label
                 htmlFor="email"
-                className="block text-xs font-medium text-gray-900 dark:text-gray-100">
+                className="block text-xs font-medium text-gray-900 dark:text-gray-100"
+              >
                 Location
               </label>
               <input
@@ -216,14 +222,13 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
                 name="location"
                 id="location"
                 value={
-                  (eventContainers && eventContainers.location) ||
-                  (formData && formData.location)
+                  (eventContainers && eventContainers.location) || (formData && formData.location)
                 }
                 readOnly={eventContainers && !!eventContainers.location}
                 className={clsx(
-                  "pl-2 py-1 block w-full border-0 p-0 text-gray-900 dark:text-gray-100 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 mb-1",
+                  'mb-1 block w-full border-0 p-0 py-1 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 dark:text-gray-100 sm:text-sm sm:leading-6',
                   {
-                    "bg-gray-400 dark:bg-gray-700 select-none":
+                    'select-none bg-gray-400 dark:bg-gray-700':
                       eventContainers && eventContainers.location,
                   }
                 )}
@@ -231,10 +236,11 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
                 onChange={formOnChange}
               />
             </div>
-            <div className="last:rounded-t-none first:rounded-b-none last:rounded-md first:rounded-md relative px-3 pt-2.5 pb-1.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-primary-400">
+            <div className="relative px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-gray-300 first:rounded-md first:rounded-b-none last:rounded-md last:rounded-t-none focus-within:z-10 focus-within:ring-2 focus-within:ring-primary-400">
               <label
                 htmlFor="email"
-                className="block text-xs font-medium text-gray-900 dark:text-gray-100">
+                className="block text-xs font-medium text-gray-900 dark:text-gray-100"
+              >
                 Email Address
               </label>
               <input
@@ -246,57 +252,57 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
                 name="email"
                 id="email"
                 value={formData && formData.email}
-                className="pl-2 py-1 block w-full border-0 p-0 text-gray-900 dark:text-gray-100 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 mb-1"
+                className="mb-1 block w-full border-0 p-0 py-1 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 dark:text-gray-100 sm:text-sm sm:leading-6"
                 placeholder="name@example.com"
                 onChange={formOnChange}
               />
             </div>
           </div>
-          {acceptingPayment && <div>
-            <p className="text-sm font-medium">Intended payment method</p>
-            <fieldset className="mt-2">
-              <div className="space-y-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-4">
-                {paymentMethod.map((payType) => (
-                  <div key={payType.value} className="flex items-center">
-                    <input
-                      id={payType.value}
-                      aria-label={payType.name}
-                      name="paymentMethod"
-                      type="radio"
-                      value={payType.value}
-                      defaultChecked={payType.value === paymentMethod[0].value}
-                      className="h-4 w-4 border-gray-300 text-primary-600 focus:ring-primary-400"
-                      onChange={formOnChange}
-                    />
-                    <label
-                      htmlFor={payType.value}
-                      className="ml-1.5 block text-sm leading-6 text-gray-800 dark:text-gray-100">
-                      {payType.name}
-                    </label>
-                  </div>
-                ))}
-              </div>
-              <p className="text-sm pl-4 text-gray-500 dark:text-gray-300">
-                *{" "}
-                {!!formData &&
-                  paymentMethod.filter(
-                    (p) => p.value === formData.paymentMethod
-                  )[0].hint}
-              </p>
-            </fieldset>
-          </div>}
+          {acceptingPayment && (
+            <div>
+              <p className="text-sm font-medium">Intended payment method</p>
+              <fieldset className="mt-2">
+                <div className="space-y-4 sm:flex sm:items-center sm:space-x-4 sm:space-y-0">
+                  {paymentMethod.map((payType) => (
+                    <div key={payType.value} className="flex items-center">
+                      <input
+                        id={payType.value}
+                        aria-label={payType.name}
+                        name="paymentMethod"
+                        type="radio"
+                        value={payType.value}
+                        defaultChecked={payType.value === paymentMethod[0].value}
+                        className="h-4 w-4 border-gray-300 text-primary-600 focus:ring-primary-400"
+                        onChange={formOnChange}
+                      />
+                      <label
+                        htmlFor={payType.value}
+                        className="ml-1.5 block text-sm leading-6 text-gray-800 dark:text-gray-100"
+                      >
+                        {payType.name}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+                <p className="pl-4 text-sm text-gray-500 dark:text-gray-300">
+                  *{' '}
+                  {!!formData &&
+                    paymentMethod.filter((p) => p.value === formData.paymentMethod)[0].hint}
+                </p>
+              </fieldset>
+            </div>
+          )}
         </div>
-        {modal === "error" && (
-          <div className="bg-red-50 text-red-600">
-            There was an error submitting your request.
-          </div>
+        {modal === 'error' && (
+          <div className="bg-red-50 text-red-600">There was an error submitting your request.</div>
         )}
         <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
           <button
             type="submit"
-            disabled={modal === "busy"}
-            className="inline-flex w-full justify-center rounded-md bg-primary-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 sm:ml-3 sm:w-auto disabled:opacity-50">
-            {modal === "busy" ? (
+            disabled={modal === 'busy'}
+            className="inline-flex w-full justify-center rounded-md bg-primary-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 disabled:opacity-50 sm:ml-3 sm:w-auto"
+          >
+            {modal === 'busy' ? (
               <>
                 Submitting ... <Spinner className="ml-2" />
               </>
@@ -306,10 +312,11 @@ export default function BookingForm({ additionalData = {}, endPoint, acceptingPa
           </button>
           <button
             type="button"
-            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hocus:bg-gray-100 sm:mt-0 sm:w-auto"
+            className="hocus:bg-gray-100 mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
             onClick={() => {
-              dispatchRedux(setModal({ status: "closed" }))
-            }}>
+              dispatchRedux(setModal({ status: 'closed' }))
+            }}
+          >
             Cancel
           </button>
         </div>
@@ -339,11 +346,11 @@ export function handleSubmit({
   endPoint: string
 }) {
   event.preventDefault()
-  dispatchRedux(setModal({ status: "busy" }))
+  dispatchRedux(setModal({ status: 'busy' }))
   fetch(endPoint, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       ...Object.fromEntries(new FormData(event.currentTarget)),
@@ -353,13 +360,13 @@ export function handleSubmit({
     .then(async (data) => {
       const json = await data.json()
       if (json.success) {
-        dispatchRedux(setModal({ status: "closed" }))
-        router.push("/confirmation")
+        dispatchRedux(setModal({ status: 'closed' }))
+        router.push('/confirmation')
       } else {
-        dispatchRedux(setModal({ status: "error" }))
+        dispatchRedux(setModal({ status: 'error' }))
       }
     })
     .catch(() => {
-      dispatchRedux(setModal({ status: "error" }))
+      dispatchRedux(setModal({ status: 'error' }))
     })
 }
