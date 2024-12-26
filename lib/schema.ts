b7306b4315f3dff3c793d50e9a155835e12c280e
Trillium Smith <spiteless@gmail.com>
2024-12-26 11:09:38 -0800

    style: Resolve eslint warnings for /lib/schema

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,30 +1,25 @@
-import { paymentMethod } from "@/data/paymentMethods"
-import { z } from "zod"
+import { paymentMethod } from '@/data/paymentMethods'
+import { z } from 'zod'
 
-const paymentMethodValues = paymentMethod.map((method) => method.value) as [
-  string,
-  ...string[]
-]
+const paymentMethodValues = paymentMethod.map((method) => method.value) as [string, ...string[]]
 
 export const AppointmentRequestSchema = z.object({
   firstName: z.string(),
   lastName: z.string(),
   email: z.string().email(),
   start: z.string().refine((value) => !Number.isNaN(Date.parse(value)), {
-    message: "Start must be a valid date.",
+    message: 'Start must be a valid date.',
   }),
   end: z.string().refine((value) => !Number.isNaN(Date.parse(value)), {
-    message: "End must be a valid date.",
+    message: 'End must be a valid date.',
   }),
   timeZone: z.string(),
   location: z.string(),
-  duration: z
-    .string()
-    .refine((value) => !Number.isNaN(Number.parseInt(value)), {
-      message: "Duration must be a valid integer.",
-    }),
+  duration: z.string().refine((value) => !Number.isNaN(Number.parseInt(value)), {
+    message: 'Duration must be a valid integer.',
+  }),
   price: z.string().refine((value) => !Number.isNaN(Number.parseInt(value)), {
-    message: "Price must be a valid integer.",
+    message: 'Price must be a valid integer.',
   }),
   phone: z.string(),
   paymentMethod: z.enum(paymentMethodValues).optional(),
@@ -38,20 +33,18 @@ export const OnSiteRequestSchema = z.object({
   lastName: z.string(),
   email: z.string().email(),
   start: z.string().refine((value) => !Number.isNaN(Date.parse(value)), {
-    message: "Start must be a valid date.",
+    message: 'Start must be a valid date.',
   }),
   end: z.string().refine((value) => !Number.isNaN(Date.parse(value)), {
-    message: "End must be a valid date.",
+    message: 'End must be a valid date.',
   }),
   timeZone: z.string(),
   location: z.string(),
-  duration: z
-    .string()
-    .refine((value) => !Number.isNaN(Number.parseInt(value)), {
-      message: "Duration must be a valid integer.",
-    }),
+  duration: z.string().refine((value) => !Number.isNaN(Number.parseInt(value)), {
+    message: 'Duration must be a valid integer.',
+  }),
   price: z.string().refine((value) => !Number.isNaN(Number.parseInt(value)), {
-    message: "Price must be a valid integer.",
+    message: 'Price must be a valid integer.',
   }),
   phone: z.string(),
   paymentMethod: z.enum(paymentMethodValues),
@@ -64,4 +57,4 @@ export const OnSiteRequestSchema = z.object({
   pricing: z.record(z.number()).optional(),
   paymentOptions: z.string(),
   leadTime: z.number(),
-});
+})
