b113d4c46459def89a465185af2f79e773230bc9
Trillium Smith <spiteless@gmail.com>
2024-12-26 12:01:31 -0800

    style: Resolve some eslint warnings for DayButton

--- components/availability/date/DayButton.tsx
+++ components/availability/date/DayButton.tsx
@@ -1,20 +1,17 @@
-import clsx from "clsx"
-import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react"
-import { twMerge } from "tailwind-merge"
+import clsx from 'clsx'
+import type { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-import Day from "@/lib/day"
+import Day from 'lib/day'
 
-import { setSelectedDate } from "@/redux/slices/availabilitySlice"
-import { useAppDispatch, useReduxAvailability } from "@/app/hooks"
+import { setSelectedDate } from 'redux/slices/availabilitySlice'
+import { useAppDispatch, useReduxAvailability } from 'app/hooks'
 
 type DayProps = {
   date: Day
   availabilityScore: number
   hasAvailability: boolean
-} & DetailedHTMLProps<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->
+} & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 
 export default function DayButton({
   date,
@@ -32,9 +29,7 @@ export default function DayButton({
 
   const isToday = date.toString() === now.toString()
 
-  const isSelected = selectedDate
-    ? date.toString() === selectedDate.toString()
-    : false
+  const isSelected = selectedDate ? date.toString() === selectedDate.toString() : false
 
   const isDisabled = !hasAvailability
 
@@ -58,55 +53,47 @@ export default function DayButton({
         // type="button"
         className={twMerge(
           clsx(
-            "transition-all flex flex-col items-center outline-primary-600 relative",
+            'relative flex flex-col items-center outline-primary-600 transition-all',
             props.className,
             {
-              "font-semibold bg-slate-300 dark:bg-slate-800 text-slate-800 dark:text-slate-200 hocus:border-primary-500 hocus:shadow-sm hocus:shadow-primary-100 hocus:z-10 border border-transparent":
+              'hocus:border-primary-500 hocus:shadow-sm hocus:shadow-primary-100 hocus:z-10 border border-transparent bg-slate-300 font-semibold text-slate-800 dark:bg-slate-800 dark:text-slate-200':
                 !isDisabled,
-              "bg-white dark:bg-slate-200 text-gray-500 dark:text-gray-500":
-                isDisabled,
+              'bg-white text-gray-500 dark:bg-slate-200 dark:text-gray-500': isDisabled,
               // "bg-primary-500": isSelected && !isToday,
               // "bg-primary-600 dark:bg-primary-600 hover:bg-primary-500": isSelected && isToday,
-              "text-white dark:text-gray-100 bg-primary-500 dark:bg-primary-600":
-                isSelected,
+              'bg-primary-500 text-white dark:bg-primary-600 dark:text-gray-100': isSelected,
             }
           )
         )}
         disabled={isDisabled}
         aria-pressed={isSelected}
         aria-disabled={isDisabled}
-        aria-label={`${isToday ? "Today" : ""} ${isDisabled ? "Unavailable" : "Available"
-          } date ${date.toString()} in calendar`}
-        {...props}>
+        aria-label={`${isToday ? 'Today' : ''} ${
+          isDisabled ? 'Unavailable' : 'Available'
+        } date ${date.toString()} in calendar`}
+        {...props}
+      >
         <div className="m-4 flex flex-col items-center justify-between leading-none">
           <p
-            className={clsx(
-              "font-semibold text-[0.55rem] leading-0 h-3 items-center flex",
-              {
-                "text-white": isSelected,
-                "text-gray-500 dark:text-gray-500": isDisabled && !isSelected,
-                "text-primary-700 dark:text-primary-600": !isSelected,
-              }
-            )}>
-            {isToday && "TODAY"}
+            className={clsx('leading-0 flex h-3 items-center text-[0.55rem] font-semibold', {
+              'text-white': isSelected,
+              'text-gray-500 dark:text-gray-500': isDisabled && !isSelected,
+              'text-primary-700 dark:text-primary-600': !isSelected,
+            })}
+          >
+            {isToday && 'TODAY'}
           </p>
-          <time className="text-base flex leading-0 items-center">
-            {date.getDay()}
-          </time>
-          <figure
-            className="flex items-center space-x-0.5 h-3 justify-center"
-            aria-hidden="true">
-            {Array.from({ length: isDisabled ? 0 : availabilityScore }).map(
-              (_, index) => (
-                <div
-                  key={`availability-bar-${index}`}
-                  className={clsx("rounded-full w-1 h-1", {
-                    "bg-white": isSelected,
-                    "bg-green-600": !isSelected,
-                  })}
-                />
-              )
-            )}
+          <time className="leading-0 flex items-center text-base">{date.getDay()}</time>
+          <figure className="flex h-3 items-center justify-center space-x-0.5" aria-hidden="true">
+            {Array.from({ length: isDisabled ? 0 : availabilityScore }).map((_, index) => (
+              <div
+                key={`availability-bar-${index}`}
+                className={clsx('h-1 w-1 rounded-full', {
+                  'bg-white': isSelected,
+                  'bg-green-600': !isSelected,
+                })}
+              />
+            ))}
           </figure>
         </div>
       </label>
