9702aa548bf9eaaadd0cbd7f1ef0516230d28e9a
Trillium Smith <spiteless@gmail.com>
2024-12-26 11:19:40 -0800

    style: Resolve eslint warnings for Spinner

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,26 +1,17 @@
-import clsx from "clsx"
-import type { SVGProps } from "react"
-import { twMerge } from "tailwind-merge"
+import clsx from 'clsx'
+import type { SVGProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-export default function Spinner({
-  className,
-  ...props
-}: SVGProps<SVGSVGElement>) {
+export default function Spinner({ className, ...props }: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      className={twMerge(clsx("animate-spin h-5 w-5 text-white", className))}
+      className={twMerge(clsx('h-5 w-5 animate-spin text-white', className))}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      {...props}>
-      <circle
-        className="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke="currentColor"
-        strokeWidth="4"
-      />
+      {...props}
+    >
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
       <path
         className="opacity-75"
         fill="currentColor"
