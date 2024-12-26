4a105640a2ff11c68b960a0a3635cc7b926a9070
Trillium Smith <spiteless@gmail.com>
2024-12-25 17:27:36 -0800

    style: Resolve eslint warnings for /onsite

--- app/onsite/page.tsx
+++ app/onsite/page.tsx
@@ -1,23 +1,19 @@
-import type { InferGetServerSidePropsType } from "next"
+import type { InferGetServerSidePropsType } from 'next'
 
-import ClientPage from "./ClientPage"
-import Template from "@/components/Template"
-import { fetchData } from "@/lib/fetch/fetchData"
-import { applyReferral } from "@/lib/posthog/applyReferral"
+import ClientPage from './ClientPage'
+import Template from 'components/Template'
+import { fetchData } from 'lib/fetch/fetchData'
+import { applyReferral } from 'lib/posthog/applyReferral'
 
 export type PageProps = InferGetServerSidePropsType<typeof fetchData>
 
-export const dynamic = "force-dynamic"
+export const dynamic = 'force-dynamic'
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: URLSearchParams
-}) {
+export default async function Page({ searchParams }: { searchParams: URLSearchParams }) {
   const { props } = await fetchData({ searchParams })
   applyReferral({ searchParams })
   return (
-    <main className="max-w-2xl sm:mx-auto mx-4 pb-24">
+    <main className="mx-4 max-w-2xl pb-24 sm:mx-auto">
       <Template title="Schedule on-site chair massage :)" />
       <ClientPage {...props} />
     </main>
