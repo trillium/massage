084f6a579d3ed1323447215d594225f3d2191287
Trillium Smith <spiteless@gmail.com>
2024-12-25 17:15:11 -0800

    style: Resolve eslint warnings for api/loc

--- app/api/loc/route.ts
+++ app/api/loc/route.ts
@@ -1,30 +1,30 @@
-import { NextRequest, NextResponse } from "next/server"
-import updateLocation from "@/lib/availability/updateLocation"
+import { NextRequest, NextResponse } from 'next/server'
+import updateLocation from 'lib/availability/updateLocation'
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams
-  console.log("🔗", searchParams)
+  console.log('🔗', searchParams)
 
-  const password = searchParams.get("password")
+  const password = searchParams.get('password')
 
   if (password !== process.env.UPDATE_LOC_PASSWORD) {
-    return new NextResponse(JSON.stringify({ error: "Access denied." }), {
+    return new NextResponse(JSON.stringify({ error: 'Access denied.' }), {
       status: 400, // NOT OK status
       headers: {
-        "Content-Type": "application/json", // Indicate the content type
+        'Content-Type': 'application/json', // Indicate the content type
       },
     })
   }
 
   // Convert searchParams to a plain object
   const paramsObj = Object.fromEntries(searchParams)
-  const location = searchParams.get("location")
+  const location = searchParams.get('location')
 
   let res
   if (location) {
     res = await updateLocation({ location })
   } else {
-    res = { error: "No location found on response"}
+    res = { error: 'No location found on response' }
   }
 
   const newObj = { ...paramsObj, res }
@@ -34,7 +34,7 @@ export async function GET(req: NextRequest) {
   return new NextResponse(JSON.stringify(paramsObj), {
     status: 200, // OK status
     headers: {
-      "Content-Type": "application/json", // Indicate the content type
+      'Content-Type': 'application/json', // Indicate the content type
     },
   })
 }
