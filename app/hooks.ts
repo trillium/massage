a6ed87127c336fa061a35b8e8bc1a9ea82f70c6a
Trillium Smith <spiteless@gmail.com>
2024-12-25 17:34:23 -0800

    style: Resolve some eslint warnings for hooks

--- app/hooks.ts
+++ app/hooks.ts
@@ -1,9 +1,9 @@
-import { useDispatch, useSelector, useStore } from "react-redux"
-import type { RootState, AppDispatch, AppStore } from "@/redux/store"
-import { selectAvailability } from "@/redux/slices/availabilitySlice"
-import { selectModal } from "@/redux/slices/modalSlice"
-import { selectFormData } from "@/redux/slices/formSlice"
-import { selectEventContainers } from "@/redux/slices/eventContainersSlice"
+import { useDispatch, useSelector, useStore } from 'react-redux'
+import type { RootState, AppDispatch, AppStore } from 'redux/store'
+import { selectAvailability } from 'redux/slices/availabilitySlice'
+import { selectModal } from 'redux/slices/modalSlice'
+import { selectFormData } from 'redux/slices/formSlice'
+import { selectEventContainers } from 'redux/slices/eventContainersSlice'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
