25fe26d39447061a6923023495834e8cda459278
Trillium Smith <spiteless@gmail.com>
2024-12-26 12:11:11 -0800

    style: Resolve eslint warnings for redux/store.ts

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,9 @@
-import { combineSlices, configureStore } from "@reduxjs/toolkit"
-import { formSlice } from "@/redux/slices/formSlice"
-import { availabilitySlice } from "@/redux/slices/availabilitySlice"
-import { modalSlice } from "@/redux/slices/modalSlice"
-import { readySlice } from "./slices/readySlice"
-import { eventContainersSlice } from "./slices/eventContainersSlice"
+import { combineSlices, configureStore } from '@reduxjs/toolkit'
+import { formSlice } from 'redux/slices/formSlice'
+import { availabilitySlice } from 'redux/slices/availabilitySlice'
+import { modalSlice } from 'redux/slices/modalSlice'
+import { readySlice } from './slices/readySlice'
+import { eventContainersSlice } from './slices/eventContainersSlice'
 
 const rootReducer = combineSlices(
   formSlice,
@@ -22,5 +22,5 @@ export const makeStore = () => {
 // Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore["getState"]>
-export type AppDispatch = AppStore["dispatch"]
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
