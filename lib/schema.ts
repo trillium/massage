dbfd069eea93ff05c1c8cee13a517eb24d7ab39a
Trillium Smith <spiteless@gmail.com>
2024-12-30 10:34:43 -0800

    feat: Refactor schema to extend from a base

--- lib/schema.ts
+++ lib/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 const paymentMethodValues = paymentMethod.map((method) => method.value) as [string, ...string[]]
 
-export const AppointmentRequestSchema = z.object({
+const BaseRequestSchema = z.object({
   firstName: z.string(),
   lastName: z.string(),
   email: z.string().email(),
@@ -22,42 +22,25 @@ export const AppointmentRequestSchema = z.object({
     message: 'Price must be a valid integer.',
   }),
   phone: z.string(),
-  paymentMethod: z.enum(paymentMethodValues).optional(),
   eventBaseString: z.string(),
   eventMemberString: z.string().optional(),
   eventContainerString: z.string().optional(),
 })
 
-export const OnSiteRequestSchema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
-  email: z.string().email(),
-  start: z.string().refine((value) => !Number.isNaN(Date.parse(value)), {
-    message: 'Start must be a valid date.',
-  }),
-  end: z.string().refine((value) => !Number.isNaN(Date.parse(value)), {
-    message: 'End must be a valid date.',
-  }),
-  timeZone: z.string(),
-  location: z.string(),
-  duration: z.string().refine((value) => !Number.isNaN(Number.parseInt(value)), {
-    message: 'Duration must be a valid integer.',
-  }),
-  price: z.string().refine((value) => !Number.isNaN(Number.parseInt(value)), {
-    message: 'Price must be a valid integer.',
-  }),
-  phone: z.string(),
+export const AppointmentRequestSchema = BaseRequestSchema.extend({
+  paymentMethod: z.enum(paymentMethodValues).optional(),
+})
+
+export const OnSiteRequestSchema = BaseRequestSchema.extend({
   paymentMethod: z.enum(paymentMethodValues),
-  eventBaseString: z.string(),
-  eventMemberString: z.string().optional(),
-  eventContainerString: z.string(),
+  eventContainerString: z.string(), // Make this field required
   allowedDurations: z.array(z.number()),
   eventName: z.string(),
   sessionDuration: z.string().optional(),
   pricing: z.record(z.number()).optional(),
   paymentOptions: z.string(),
   leadTime: z.number(),
-})
+});
 
-export type AppointmentRequestType = z.infer<typeof AppointmentRequestSchema>;
-export type OnSiteRequestType = z.infer<typeof OnSiteRequestSchema>;
+export type AppointmentRequestType = z.infer<typeof AppointmentRequestSchema>
+export type OnSiteRequestType = z.infer<typeof OnSiteRequestSchema>
