223073577b2544d869c541e41aa3ae3909253a75
Trillium Smith <spiteless@gmail.com>
2024-12-30 10:56:07 -0800

    feat: Add StoreProvider to ThemeProviders

--- app/ThemeProviders.tsx
+++ app/ThemeProviders.tsx
@@ -2,15 +2,18 @@
 
 import { CSPostHogProvider } from 'context/AnalyticsContext'
 import { ThemeProvider } from 'next-themes'
+import StoreProvider from 'app/StoreProvider'
 
 const theme = 'system'
 
 export function ThemeProviders({ children }: { children: React.ReactNode }) {
   return (
     <CSPostHogProvider>
-      <ThemeProvider attribute="class" defaultTheme={theme} enableSystem>
-        {children}
-      </ThemeProvider>
+      <StoreProvider>
+        <ThemeProvider attribute="class" defaultTheme={theme} enableSystem>
+          {children}
+        </ThemeProvider>
+      </StoreProvider>
     </CSPostHogProvider>
   )
 }
