e26538041e6e7246b67e23ac497aa58674d4b258
Trillium Smith <spiteless@gmail.com>
2025-01-03 12:01:28 -0800

    fix: Resolve searchParams as promise in app/reviews/rate

--- app/reviews/rate/page.tsx
+++ app/reviews/rate/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ClientPage from './ClientPage'
-import { decode } from 'lib/hashServer'
+import {decode} from 'lib/hashServer'
 
-export default async function Page({ searchParams }: { searchParams: Object }) {
-  const { validated, data } = await decode(searchParams)
-  const { date, start, end, firstName, lastName } = data
+export default async function Page({searchParams}: {searchParams: Promise<URLSearchParams>}) {
+  const resolvedParams = await searchParams
+  const {validated, data} = await decode(resolvedParams)
+  const {date, start, end, firstName, lastName} = data
 
-  return <ClientPage {...{ validated, date, start, end, firstName, lastName, ...data }} />
+  return <ClientPage {...{validated, date, start, end, firstName, lastName, ...data}} />
 }
