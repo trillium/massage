aec09c71f48d08d37b25190710dfd2a620f34d7e
Trillium Smith <spiteless@gmail.com>
2025-01-03 11:59:05 -0800

    fix: Resolve searchParams as promise in app/onsite/page

--- app/onsite/page.tsx
+++ app/onsite/page.tsx
@@ -1,18 +1,19 @@
-import type { InferGetServerSidePropsType } from 'next'
+import type {InferGetServerSidePropsType} from 'next'
 
 import ClientPage from './ClientPage'
 import Template from 'components/Template'
-import { fetchData } from 'lib/fetch/fetchData'
+import {fetchData} from 'lib/fetch/fetchData'
 
 export type PageProps = InferGetServerSidePropsType<typeof fetchData>
 
 export const dynamic = 'force-dynamic'
 
-export default async function Page({ searchParams }: { searchParams: URLSearchParams }) {
-  const { props } = await fetchData({ searchParams })
+export default async function Page({searchParams}: {searchParams: Promise<URLSearchParams>}) {
+  const resolvedParams = await searchParams
+  const {props} = await fetchData({searchParams: resolvedParams})
   return (
     <main className="mx-4 max-w-2xl pb-24 sm:mx-auto">
-      <Template title="Schedule on-site chair massage :)" />
+      <Template title="Book a session with Trillium :)" />
       <ClientPage {...props} />
     </main>
   )
