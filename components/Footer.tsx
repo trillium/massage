eb893143209dc6daf0281d80efbdbd251235936a
Trillium Smith <spiteless@gmail.com>
2025-01-03 21:13:05 -0800

    feat: Comment out blog base link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,9 +27,9 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          {/* <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog"> */}
+          {/* Future footer storage */}
+          {/* </Link> */}
         </div>
       </div>
     </footer>
