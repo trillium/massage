358c45a12f856c142421d1f2ce60e1000b5dc3be
Trillium Smith <spiteless@gmail.com>
2025-01-07 13:49:05 -0800

    chore: Move template blog posts into /storage

---
+++ storage/headerNavLinks.ts
@@ -0,0 +1,11 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/book', title: 'Booking' },
+  // { href: '/blog', title: 'Blog' },
+  // { href: '/tags', title: 'Tags' },
+  { href: '/reviews', title: 'Reviews' },
+  { href: '/about', title: 'About' },
+  { href: '/faq', title: 'FAQ' },
+]
+
+export default headerNavLinks
