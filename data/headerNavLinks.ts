abd8ed4a5dc8a0c8f5a99dc893b28647b7de67d8
Trillium Smith <spiteless@gmail.com>
2025-01-09 21:03:38 -0800

    chore: Rename Book to Booking in headerNavLinks

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/book', title: 'Book' },
+  { href: '/book', title: 'Booking' },
   // { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
   { href: '/reviews', title: 'Reviews' },
