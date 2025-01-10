3bf0d57ce73e6754b1267be8901290a37b37c17b
Trillium Smith <spiteless@gmail.com>
2025-01-09 21:04:14 -0800

    feat: Add scheduler to front page of app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
+import Template from '@/components/Template'
+import { fetchData } from 'lib/fetch/fetchData'
+import ClientPage from './ClientPage'
 import Main from './Main'
 
-export default async function Page() {
+export default async function Page({ searchParams }: { searchParams: Promise<URLSearchParams> }) {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+  const resolvedParams = await searchParams
+  const { props } = await fetchData({ searchParams: resolvedParams })
+  return (
+    <>
+      <Template title="Book a session with Trillium :)" />
+      <ClientPage {...props} />
+      <Main posts={posts} />
+    </>
+  )
 }
