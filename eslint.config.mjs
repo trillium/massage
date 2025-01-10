f84183e38f4ea33ccba902e4ce35b0bcd02f2c55
Trillium Smith <spiteless@gmail.com>
2025-01-09 21:05:52 -0800

    refactor: Define eslint config object then export

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,11 @@ import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import globals from 'globals'
 import tsParser from '@typescript-eslint/parser'
 import path from 'node:path'
-import {fileURLToPath} from 'node:url'
+import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
-import {FlatCompat} from '@eslint/eslintrc'
+import { FlatCompat } from '@eslint/eslintrc'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -12,7 +14,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-export default [
+const eslintConfig = [
   {
     ignores: [],
   },
@@ -67,4 +69,7 @@ export default [
       '@typescript-eslint/ban-ts-comment': 'off',
     },
   },
+  prettierConfig,
 ]
+
+export default eslintConfig
