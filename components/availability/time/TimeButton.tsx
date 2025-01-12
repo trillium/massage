20770e6290818c4bf06862537c70bba5e37dea58
Trillium Smith <spiteless@gmail.com>
2025-01-11 17:07:09 -0800

    feat: Add active state to TimeButton

--- components/availability/time/TimeButton.tsx
+++ components/availability/time/TimeButton.tsx
@@ -9,12 +9,19 @@ import { setSelectedTime } from '@/redux/slices/availabilitySlice'
 import { setModal } from '@/redux/slices/modalSlice'
 import { useAppDispatch, useReduxAvailability } from '@/redux/hooks'
 import { clearEventContainers, setEventContainers } from '@/redux/slices/eventContainersSlice'
+import { formatDatetimeToString } from '@/lib/helpers'
 
 type TimeProps = {
   time: DateTimeInterval
+  active: boolean
 } & { location?: string } & DetailedHTMLProps<HTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 
-export default function TimeButton({ time: { start, end }, location, ...props }: TimeProps) {
+export default function TimeButton({
+  time: { start, end },
+  active,
+  location,
+  ...props
+}: TimeProps) {
   const { timeZone } = useReduxAvailability()
   const dispatchRedux = useAppDispatch()
 
@@ -22,16 +29,20 @@ export default function TimeButton({ time: { start, end }, location, ...props }:
     <button
       type="button"
       className={clsx(
-        'rounded-md border border-slate-300 bg-white px-3 py-2 shadow-sm transition-all',
-        'text-sm font-semibold text-gray-900',
+        'rounded-md border border-slate-300 px-3 py-2 shadow-sm transition-all',
+        'text-sm text-gray-900',
         'hocus:bg-primary-50/20 hocus:shadow-sm hocus:shadow-primary-100 hocus:border-primary-500 dark:hocus:text-gray-200',
-        'outline-primary-600 active:-mb-0.5  active:mt-0.5'
+        'outline-primary-600 active:-mb-0.5  active:mt-0.5',
+        {
+          'bg-primary-500 font-bold text-white': active,
+          'bg-white font-semibold text-gray-900': !active,
+        }
       )}
       onClick={() => {
         dispatchRedux(
           setSelectedTime({
-            start: format(start, "yyyy-MM-dd'T'HH:mm:ssXXX"),
-            end: format(end, "yyyy-MM-dd'T'HH:mm:ssXXX"),
+            start: formatDatetimeToString(start),
+            end: formatDatetimeToString(end),
           })
         )
         if (location) {
--- components/availability/time/TimeList.tsx
+++ components/availability/time/TimeList.tsx
@@ -1,19 +1,32 @@
+import { useReduxAvailability } from '@/redux/hooks'
 import TimeButton from './TimeButton'
 import type { DateTimeIntervalAndLocation } from '@/lib/types'
+import { formatDatetimeToString } from '@/lib/helpers'
 
 type TimeListProps = {
   availability: DateTimeIntervalAndLocation[]
 }
 export default function TimeList({ availability }: TimeListProps) {
+  const { selectedTime, timeZone } = useReduxAvailability()
+
+  const timeSignature = (selectedTime?.start ?? '') + (selectedTime?.end ?? '')
+
   return (
     <div className="grid grid-cols-2 gap-2">
-      {availability?.map(({ start, end, location }) => (
-        <TimeButton
-          key={start.toISOString() + end.toISOString()}
-          time={{ start, end }}
-          location={location}
-        />
-      ))}
+      {availability?.map(({ start, end, location }) => {
+        const isActive = selectedTime
+          ? formatDatetimeToString(start) + formatDatetimeToString(end) === timeSignature
+          : false
+
+        return (
+          <TimeButton
+            key={start.toISOString() + end.toISOString()}
+            active={isActive}
+            time={{ start, end }}
+            location={location}
+          />
+        )
+      })}
     </div>
   )
 }
