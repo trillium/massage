7c6b9198f44924b387f4736c20c565f781d67119
Trillium Smith <spiteless@gmail.com>
2025-01-11 17:15:29 -0800

    feat: Create ClientPage for /

---
+++ app/ClientPage.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import type { InferGetServerSidePropsType } from 'next'
+
+import AvailabilityPicker from 'components/availability/AvailabilityPicker'
+import { PricingWrapper } from 'components/availability/PricingWrapper'
+import { DEFAULT_PRICING } from 'config'
+
+import PageProps from './page'
+import BookingForm from 'components/booking/BookingForm'
+
+const pricing = DEFAULT_PRICING
+
+// Need to refactor fetchData so it's easier to extend to other pages
+
+function Page({
+  start,
+  end,
+  busy,
+  selectedDate,
+  duration,
+}: InferGetServerSidePropsType<typeof PageProps>) {
+  const { slots, pickerProps } = PricingWrapper({
+    start,
+    end,
+    busy,
+    pricing,
+    selectedDate,
+    duration,
+  })
+
+  return (
+    <>
+      <AvailabilityPicker slots={slots} pickerProps={pickerProps}>
+        <BookingForm endPoint="api/request" />
+      </AvailabilityPicker>
+    </>
+  )
+}
+
+export default Page
