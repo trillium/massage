90bc6ad60094c03ef09c96b11eba710deb3c4331
Trillium Smith <spiteless@gmail.com>
2025-01-29 02:32:08 -0800

    feat: Add new utilities to onsite/page

--- app/onsite/page.tsx
+++ app/onsite/page.tsx
@@ -3,7 +3,13 @@ import type { InferGetServerSidePropsType } from 'next'
 import ClientPage from './ClientPage'
 import Template from 'components/Template'
 import { fetchData } from 'lib/fetch/fetchData'
-import { SearchParamsType } from '@/lib/types'
+import { AllowedDurationsType, SearchParamsType } from '@/lib/types'
+import { dayFromString } from '@/lib/dayAsObject'
+import { createSlots } from '@/lib/availability/createSlots'
+import { DEFAULT_DURATION, LEAD_TIME } from 'config'
+import { InitialUrlUtility } from '@/components/utilities/InitialUrlUtility'
+import { UrlUpdateUtility } from '@/components/utilities/UrlUpdateUtility'
+import { UpdateSlotsUtility } from '@/components/utilities/UpdateSlotsUtility'
 
 export type PageProps = InferGetServerSidePropsType<typeof fetchData>
 
@@ -12,10 +18,43 @@ export const dynamic = 'force-dynamic'
 export default async function Page({ searchParams }: { searchParams: Promise<SearchParamsType> }) {
   const resolvedParams = await searchParams
   const { props } = await fetchData({ searchParams: resolvedParams })
+
+  const duration =
+    resolvedParams.duration !== undefined ? Number(resolvedParams.duration) : DEFAULT_DURATION
+
+  const start = dayFromString(props.start)
+  const end = dayFromString(props.end)
+
+  const slots = createSlots({ ...props, duration, leadTime: LEAD_TIME, start, end })
+
+  const configuration = null
+
+  const { selectedDate } = props
+
   return (
-    <main>
+    <>
       <Template title="Book a session with Trillium :)" />
       <ClientPage {...props} />
-    </main>
+
+      <InitialUrlUtility
+        configSliceData={configuration}
+        selectedDate={selectedDate}
+        duration={duration}
+        allowedDurations={allowedDurations}
+        slots={slots}
+      />
+      <UrlUpdateUtility />
+      <UpdateSlotsUtility busy={props.busy} start={start} end={end} />
+    </>
   )
 }
+
+const allowedDurations: AllowedDurationsType = [
+  60 * 1,
+  60 * 1.5,
+  60 * 2,
+  60 * 2.5,
+  60 * 3,
+  60 * 3.5,
+  60 * 4,
+]
