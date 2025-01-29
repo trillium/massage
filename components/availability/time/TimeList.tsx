1873928c0ee3930b7edbc5a7291aa69a3f69bb36
Trillium Smith <spiteless@gmail.com>
2025-01-29 01:13:01 -0800

    feat: Move availabilityByDate calc into TimeList (Can be refactored later)

--- components/availability/time/TimeList.tsx
+++ components/availability/time/TimeList.tsx
@@ -1,26 +1,48 @@
+'use client'
+
 import { useReduxAvailability } from '@/redux/hooks'
 import TimeButton from './TimeButton'
-import type { DateTimeIntervalAndLocation } from '@/lib/types'
-import { formatDatetimeToString } from '@/lib/helpers'
+import type { StringDateTimeIntervalAndLocation } from '@/lib/types'
 
-type TimeListProps = {
-  availability: DateTimeIntervalAndLocation[]
-}
-export default function TimeList({ availability }: TimeListProps) {
+import { format } from 'date-fns-tz'
+
+export default function TimeList({}) {
+  const { slots: slotsRedux, selectedDate } = useReduxAvailability()
   const { selectedTime, timeZone } = useReduxAvailability()
 
+  const slots = slotsRedux || []
+
   const timeSignature = (selectedTime?.start ?? '') + (selectedTime?.end ?? '')
+
+  let maximumAvailability = 0
+  const availabilityByDate = slots.reduce<Record<string, StringDateTimeIntervalAndLocation[]>>(
+    (acc, slot) => {
+      // Gives us the same YYYY-MM-DD format as Day.toString()
+      const date = format(slot.start, 'yyyy-MM-dd', { timeZone })
+
+      if (!acc[date]) {
+        acc[date] = []
+      }
+      acc[date].push(slot)
+
+      if (acc[date].length > maximumAvailability) {
+        maximumAvailability = acc[date].length
+      }
+      return acc
+    },
+    {}
+  )
+
+  const availability = selectedDate ? availabilityByDate[selectedDate.toString()] : []
 
   return (
     <div className="grid grid-cols-2 gap-2">
       {availability?.map(({ start, end, location }) => {
-        const isActive = selectedTime
-          ? formatDatetimeToString(start) + formatDatetimeToString(end) === timeSignature
-          : false
+        const isActive = selectedTime ? start + end === timeSignature : false
 
         return (
           <TimeButton
-            key={start.toISOString() + end.toISOString()}
+            key={start + end}
             active={isActive}
             time={{ start, end }}
             location={location}
