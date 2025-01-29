0537a1de02a762e32829d569af63eabff91f0f0b
Trillium Smith <spiteless@gmail.com>
2025-01-28 21:46:08 -0800

    feat: Create createSlots function for refactor out of PricingWrapper

---
+++ lib/availability/createSlots.ts
@@ -0,0 +1,52 @@
+import { OWNER_AVAILABILITY, LEAD_TIME } from 'config'
+import getAvailability from 'lib/availability/getAvailability'
+import getPotentialTimes from 'lib/availability/getPotentialTimes'
+import { mapStringsToDates } from 'lib/availability/helpers'
+
+import type {
+  GoogleCalendarV3Event,
+  StringDateTimeIntervalAndLocation,
+  StringInterval,
+  DayWithStartEnd,
+} from 'lib/types'
+
+type CreateSlotsType = {
+  start: DayWithStartEnd
+  end: DayWithStartEnd
+  busy: StringInterval[]
+  leadTime: number
+  duration: number
+  containers: GoogleCalendarV3Event[]
+}
+
+export function createSlots({
+  start,
+  end,
+  busy,
+  leadTime = LEAD_TIME,
+  duration,
+  containers,
+}: CreateSlotsType): StringDateTimeIntervalAndLocation[] {
+  const startOfInterval = start.start
+  const endOfInterval = end.end
+
+  const potential = getPotentialTimes({
+    start,
+    end,
+    duration: duration,
+    availabilitySlots: OWNER_AVAILABILITY,
+    containers: containers,
+  })
+
+  const offers = getAvailability({
+    busy: mapStringsToDates(busy),
+    potential,
+    leadTime,
+  })
+
+  const slots = offers.filter((slot, index) => {
+    return slot.start >= startOfInterval && slot.end <= endOfInterval
+  })
+
+  return slots
+}
