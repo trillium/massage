4977a58a995d64308ec1447217f09d3aaab6f16e
Trillium Smith <spiteless@gmail.com>
2025-01-29 01:58:30 -0800

    refactor: Remove PricingWrapper and ClientPage from app/page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,21 @@ import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Template from '@/components/Template'
 import { fetchData } from 'lib/fetch/fetchData'
-import ClientPage from './ClientPage'
 import Main from './Main'
 import Hero from '@/components/hero/Hero'
 import siteMetadata from 'storage/siteMetadata'
 import Masonry from '@/components/masonry/Masonry'
 import { SearchParamsType } from '@/lib/types'
+import { ALLOWED_DURATIONS, DEFAULT_DURATION, DEFAULT_PRICING, LEAD_TIME } from 'config'
+import BookingForm from '@/components/booking/BookingForm'
+import DurationPicker from '@/components/availability/controls/DurationPicker'
+import Calendar from '@/components/availability/date/Calendar'
+import TimeList from '@/components/availability/time/TimeList'
+import { dayFromString } from '@/lib/dayAsObject'
+import { createSlots } from '@/lib/availability/createSlots'
+import { InitialUrlUtility } from '@/components/utilities/InitialUrlUtility'
+import { UrlUpdateUtility } from '@/components/utilities/UrlUpdateUtility'
+import { UpdateSlotsUtility } from '@/components/utilities/UpdateSlotsUtility'
 
 const { avatar } = siteMetadata
 const mapData = '/static/images/foo/service-area.jpg'
@@ -20,7 +29,32 @@ export default async function Page({ searchParams }: { searchParams: Promise<Sea
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
   const resolvedParams = await searchParams
+  const duration =
+    resolvedParams.duration !== undefined ? Number(resolvedParams.duration) : DEFAULT_DURATION
+
   const { props } = await fetchData({ searchParams: resolvedParams })
+
+  const start = dayFromString(props.start)
+  const end = dayFromString(props.end)
+
+  const slots = createSlots({ ...props, duration, leadTime: LEAD_TIME, start, end })
+
+  const configuration = null
+
+  const pricing = DEFAULT_PRICING
+  const durationString = `${duration || '##'} minute session`
+  const paymentString = ' - $' + pricing[duration]
+  const combinedString = durationString + paymentString
+
+  const durationProps = {
+    title: combinedString,
+    price: pricing,
+    duration: duration,
+    allowedDurations: ALLOWED_DURATIONS,
+  }
+
+  const { selectedDate } = props
+
   return (
     <>
       <Hero
@@ -37,7 +71,23 @@ export default async function Page({ searchParams }: { searchParams: Promise<Sea
         imageLeft
       />
       <Template title="Book a massage with Trillium :)" />
-      <ClientPage {...props} />
+      <BookingForm endPoint="api/request" />
+      <div className="flex flex-col space-y-8">
+        <DurationPicker {...durationProps} />
+        <Calendar />
+        <TimeList />
+      </div>
+
+      <InitialUrlUtility
+        configSliceData={configuration}
+        selectedDate={selectedDate}
+        duration={duration}
+        allowedDurations={ALLOWED_DURATIONS}
+        slots={slots}
+      />
+      <UrlUpdateUtility />
+      <UpdateSlotsUtility busy={props.busy} start={start} end={end} />
+
       {!!posts.length && <Main posts={posts} />}
     </>
   )
