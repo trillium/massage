31540c29fdf86c756bd1b174caab487093566dfc
Trillium Smith <spiteless@gmail.com>
2025-01-30 15:29:36 -0800

    chore: Add jsdoc to formatDatetimeToString

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,5 +1,11 @@
 import { format } from 'date-fns'
 
-export function formatDatetimeToString(datetimeObj: Date) {
+/**
+ * Formats a Date object to a string in the format "yyyy-MM-dd'T'HH:mm:ssXXX".
+ *
+ * @param {Date} datetimeObj - The Date object to format.
+ * @returns {string} The formatted date string.
+ */
+export function formatDatetimeToString(datetimeObj: Date): string {
   return format(datetimeObj, "yyyy-MM-dd'T'HH:mm:ssXXX")
 }
