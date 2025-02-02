2af6e8d32354703da1abf5cc032dab833a7dd524
Trillium Smith <spiteless@gmail.com>
2025-02-01 23:57:07 -0800

    refactor: Remove validateSearchParams code from fetch fns

--- lib/fetch/fetchContainersByQuery.ts
+++ lib/fetch/fetchContainersByQuery.ts
@@ -1,70 +1,14 @@
-import { z } from 'zod'
-import {
-  DEFAULT_APPOINTMENT_INTERVAL,
-  DEFAULT_DURATION,
-  DEFAULT_PRICING,
-  VALID_DURATIONS,
-} from 'config'
 import Day from 'lib/day'
 import { getEventsBySearchQuery } from '../availability/getEventsBySearchQuery'
 import { GoogleCalendarV3Event, SearchParamsType } from 'lib/types'
 import { loadData } from 'lib/dataLoading'
 
 export async function fetchContainersByQuery({
-  searchParams,
   query,
 }: {
   searchParams: SearchParamsType
   query: string
 }) {
-  const schema = z.object({
-    duration: z
-      .enum([
-        ...(VALID_DURATIONS.map(String) as [string, ...string[]]),
-        DEFAULT_APPOINTMENT_INTERVAL.toString(),
-      ])
-      .optional()
-      .default(String(DEFAULT_DURATION))
-      .transform(Number),
-    timeZone: z.string().optional(),
-    selectedDate: z
-      .string()
-      .regex(/^\d{4}-\d{2}-\d{2}$/u)
-      .optional(),
-  })
-
-  let duration: number | undefined = undefined
-  let timeZone: string | undefined = undefined
-  let selectedDate: string | undefined = undefined
-
-  try {
-    const parsedParams = schema.parse(searchParams)
-    try {
-      duration = parsedParams.duration
-    } catch {
-      duration = undefined
-    }
-
-    try {
-      timeZone = parsedParams.timeZone
-    } catch {
-      timeZone = undefined
-    }
-
-    try {
-      selectedDate = parsedParams.selectedDate
-    } catch {
-      selectedDate = undefined
-    }
-  } catch (error) {
-    console.error('Failed to parse searchParams:', error)
-  }
-
-  if (duration == undefined) {
-    // if validation faisl
-    duration = DEFAULT_DURATION
-  }
-
   // Offer three weeks of availability predefined event based container availability.
   const start = Day.todayWithOffset(0)
   const end = Day.todayWithOffset(21)
@@ -120,15 +64,9 @@ export async function fetchContainersByQuery({
   })
 
   return {
-    props: {
-      start: start.toString(),
-      end: end.toString(),
-      busy: busyQuery,
-      containers: containersMapped,
-      duration,
-      ...(timeZone && { timeZone }),
-      ...(selectedDate && { selectedDate }),
-      price: DEFAULT_PRICING[DEFAULT_DURATION],
-    },
+    start: start.toString(),
+    end: end.toString(),
+    busy: busyQuery,
+    containers: containersMapped,
   }
 }
