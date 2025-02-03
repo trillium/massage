5d462cdf2ab6d1d90996536f2e3340dc006a5197
Trillium Smith <spiteless@gmail.com>
2025-02-02 20:42:05 -0800

    feat: Create GeneratePriceAtom component

---
+++ components/utilities/GeneratePriceAtom.tsx
@@ -0,0 +1,33 @@
+import { DiscountType } from '@/lib/types'
+import clsx from 'clsx'
+
+type GeneratePriceType = { price: number; discount?: DiscountType | null }
+
+export function GeneratePrice({ price, discount }: GeneratePriceType) {
+  let discountPrice
+  if (discount) {
+    const { amountDollars, amountPercent } = discount
+    if (discount?.type === 'dollar' && amountDollars !== undefined) {
+      discountPrice = price - amountDollars
+    } else if (discount?.type === 'percent' && amountPercent !== undefined) {
+      discountPrice = price * (1 - amountPercent)
+    }
+  }
+  return (
+    <>
+      <span
+        className={clsx({
+          'line-through decoration-red-500 decoration-2': !!discount,
+        })}
+      >
+        ${price}
+      </span>
+      {!!discount && (
+        <>
+          {' '}
+          {'->  '} <span>${discountPrice}</span>
+        </>
+      )}
+    </>
+  )
+}
