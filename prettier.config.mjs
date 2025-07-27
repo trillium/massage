eddd3c22ec1b6b1690f2045b664bf5f1e045c234
Trillium Smith <trillium@trilliumsmith.com>
2025-07-27 13:11:01 -0700

    ci: Change prettier from .js to .mjs

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,10 +1,10 @@
-module.exports = {
+export default {
   semi: false,
   singleQuote: true,
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   bracketSpacing: true,
-  plugins: ['prettier-plugin-tailwindcss'],
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+};
