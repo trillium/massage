2ac51f59ef1ce1ec04d9bfe28fa2364b0ab41e1e
Trillium Smith <trillium@trilliumsmith.com>
2025-07-27 14:33:43 -0700

    refactor: Update reviews page to resolve typing problem

--- app/reviews/rate/page.tsx
+++ app/reviews/rate/page.tsx
@@ -4,8 +4,24 @@ import { decode } from 'lib/hashServer'
 
 export default async function Page({ searchParams }: { searchParams: Promise<URLSearchParams> }) {
   const resolvedParams = await searchParams
-  const { validated, data } = await decode(resolvedParams)
+  // Convert URLSearchParams to plain object for decode function
+  const paramsObject: { [key: string]: string } = {}
+  resolvedParams.forEach((value, key) => {
+    paramsObject[key] = value
+  })
+
+  const { validated, data } = await decode(paramsObject)
   const { date, start, end, firstName, lastName } = data
 
-  return <ClientPage {...{ validated, date, start, end, firstName, lastName, ...data }} />
+  const clientPageProps = {
+    validated,
+    date: date as string,
+    start: start as string,
+    end: end as string,
+    firstName: firstName as string,
+    lastName: lastName as string,
+    ...data,
+  }
+
+  return <ClientPage {...clientPageProps} />
 }
