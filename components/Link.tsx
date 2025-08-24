e6d36344ab6447ed5b9ffa23c69969f66f8f9036
Trillium Smith <trillium@trilliumsmith.com>
2025-08-23 20:22:16 -0700

    feat: Add classes prop to Link - Update Link to use clsx

--- components/Link.tsx
+++ components/Link.tsx
@@ -2,21 +2,32 @@
 import Link from 'next/link'
 import type { LinkProps } from 'next/link'
 import { AnchorHTMLAttributes } from 'react'
+import clsx from 'clsx'
 
-const CustomLink = ({ href, ...rest }: LinkProps & AnchorHTMLAttributes<HTMLAnchorElement>) => {
+const CustomLink = ({
+  href,
+  classes,
+  ...rest
+}: LinkProps & AnchorHTMLAttributes<HTMLAnchorElement> & { classes?: string }) => {
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
 
   if (isInternalLink) {
-    return <Link className="break-words" href={href} {...rest} />
+    return <Link className={clsx('break-words', classes)} href={href} {...rest} />
   }
 
   if (isAnchorLink) {
-    return <a className="break-words" href={href} {...rest} />
+    return <a className={clsx('break-words', classes)} href={href} {...rest} />
   }
 
   return (
-    <a className="break-words" target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+    <a
+      className={clsx('break-words', classes)}
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      {...rest}
+    />
   )
 }
 
