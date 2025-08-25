b4d03d163d47c82c27d00d76f9f883d0a2778a35
Trillium Smith <trillium@trilliumsmith.com>
2025-08-25 10:41:51 -0700

    fix(analytics): Update posthog api_hosts to use /ingest by default

--- context/AnalyticsContext.tsx
+++ context/AnalyticsContext.tsx
@@ -6,7 +6,7 @@ import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined' && !process.env.NEXT_PUBLIC_DISABLE_POSTHOG) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: '/ingest',
     ui_host: 'https://us.posthog.com',
     person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
     custom_campaign_params: ['ref'],
