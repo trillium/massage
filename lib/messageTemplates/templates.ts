6915b6c821edcbc03b9a952ae5a757f5316b2a21
Trillium Smith <trillium@trilliumsmith.com>
2025-09-14 10:25:36 -0700

    chore: remove legacy messageTemplates folder

--- lib/messageTemplates/templates.ts
+++
@@ -1,7 +0,0 @@
-import { AppointmentProps, LocationObject } from '../types'
-import { flattenLocation } from '@/lib/helpers/locationHelpers'
-import { generateSecureMyEventsUrlServer } from '@/lib/generateSecureMyEventsUrl'
-
-const templates = {}
-
-export default templates
