8ac2bb73b769cb35cf87060b2b2f648067457dbb
Trillium Smith <trillium@trilliumsmith.com>
2025-12-09 21:13:13 -0800

    style(button): Add white hover to BookSessionButton for light mode

--- components/BookSessionButton.tsx
+++ components/BookSessionButton.tsx
@@ -11,7 +11,7 @@ const BookSessionButton: React.FC<BookSessionButtonProps> = ({ href, title }) =>
     href={href}
     className="group border-primary-500 dark:border-primary-400 relative mb-4 inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full border-4 pr-14 pl-6 font-medium"
   >
-    <div className="bg-primary-500 group-hover:from-primary-400 group-hover:via-primary-500 group-hover:to-primary-500 absolute right-0 -z-0 inline-flex h-12 w-12 items-center justify-end rounded-full transition-[width] group-hover:w-[calc(100%)] group-hover:bg-gradient-to-r">
+    <div className="bg-primary-500 group-hover:from-primary-400 group-hover:via-primary-500 group-hover:to-primary-500 absolute right-0 -z-0 inline-flex h-12 w-12 items-center justify-end rounded-full transition-[width] group-hover:w-[calc(100%)] group-hover:bg-gradient-to-r group-hover:text-white">
       <div className="mr-3.5 flex items-center justify-center">
         <svg
           width="15"
@@ -19,7 +19,7 @@ const BookSessionButton: React.FC<BookSessionButtonProps> = ({ href, title }) =>
           viewBox="0 0 15 15"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          className="stroke h-5 w-5 stroke-black dark:stroke-white"
+          className="stroke h-5 w-5 stroke-black group-hover:stroke-white dark:stroke-white"
         >
           <path
             d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
@@ -30,7 +30,9 @@ const BookSessionButton: React.FC<BookSessionButtonProps> = ({ href, title }) =>
         </svg>
       </div>
     </div>
-    <span className="text-black-500 z-0 pr-2 text-xl font-bold dark:text-white">{title}</span>
+    <span className="text-black-500 z-0 pr-2 text-xl font-bold group-hover:text-white dark:text-white">
+      {title}
+    </span>
   </Link>
 )
 
