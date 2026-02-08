b137d21c2295c28af0899869c28f80656f4a691a
Trillium Smith <trillium@trilliumsmith.com>
2026-02-08 08:43:22 -0800

    refactor(data): extract Trillium Massage ratings into own file

--- data/ratings.ts
+++ data/ratings.ts
@@ -1,11 +1,12 @@
 import type { ReviewType } from '@/lib/types'
 import sootheRatings from './ratings-soothe'
+import trilliumRatings from './ratings-trillium'
 import airbnbRatings from './ratings-airbnb'
 
 export const links = {
   airbnb: 'https://www.airbnb.com/services/6527842?modal=reviews',
 }
 
-const ratings: ReviewType[] = [...sootheRatings, ...airbnbRatings]
+const ratings: ReviewType[] = [...sootheRatings, ...trilliumRatings, ...airbnbRatings]
 
 export default ratings
