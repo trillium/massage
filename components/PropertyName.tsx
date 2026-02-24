bcc2722506de242a8b31a7a181a9a0879df66605
Trillium Smith <trillium@trilliumsmith.com>
2026-02-24 13:15:07 -0800

    chore: remove unused client slug and promotion components

--- components/PropertyName.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useContext } from 'react'
-import { ClientSlugContext } from '@/lib/ClientSlugContext'
-
-export default function PropertyName() {
-  const { propertyName } = useContext(ClientSlugContext)
-  return <>{propertyName || 'Your Property'}</>
-}
--- components/PropertyNameVar.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-interface PropertyNameVarProps {
-  children: string
-}
-
-export default function PropertyNameVar({ children }: PropertyNameVarProps) {
-  return <>{children}</>
-}
