79ac8eae5bb205b99c16279692670f3ea007985e
Trillium Smith <trillium@trilliumsmith.com>
2026-02-24 13:48:19 -0800

    feat(mdx): register AboutLayout and BookingPreview components

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -5,6 +5,8 @@ import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
+import { AboutLayout } from './landingPage/AboutSection'
+import BookingPreview from './BookingPreview'
 
 export const components: MDXComponents = {
   Image,
@@ -13,4 +15,6 @@ export const components: MDXComponents = {
   pre: Pre,
   table: TableWrapper,
   BlogNewsletterForm,
+  AboutLayout,
+  BookingPreview,
 }
