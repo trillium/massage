05b59d291ab09ce244ea2ab70b528d219a89fdbd
Trillium Smith <trillium@trilliumsmith.com>
2026-05-09 21:18:10 -0700

    fix(config): single Sentry init in instrumentation-client, add umami to CSP

--- next.config.js
+++ next.config.js
@@ -11,11 +11,11 @@ const withBundleAnalyzer = bundleAnalyzer({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://analytics.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
-  connect-src 'self' https://qrvuazoacpolbojkimyu.supabase.co wss://qrvuazoacpolbojkimyu.supabase.co https://*.ingest.sentry.io;
+  connect-src 'self' https://qrvuazoacpolbojkimyu.supabase.co wss://qrvuazoacpolbojkimyu.supabase.co https://*.ingest.sentry.io https://analytics.umami.is;
   font-src 'self';
   frame-src 'self';
   worker-src 'self' blob:;
