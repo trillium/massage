041b958ee8a3f0955302d4c713eb1fcae7dcac90
Trillium Smith <trillium@trilliumsmith.com>
2026-06-12 16:05:40 -0700

    fix(og): force-include table images in dynamic route serverless bundle via outputFileTracingIncludes

--- next.config.js
+++ next.config.js
@@ -77,6 +77,9 @@ const nextConfig = () => {
     outputFileTracingExcludes: {
       '*': ['./node_modules/googleapis/**', './node_modules/maplibre-gl/**'],
     },
+    outputFileTracingIncludes: {
+      '/[bookingSlug]/opengraph-image': ['./public/static/images/table/**'],
+    },
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     images: {
       remotePatterns: [
