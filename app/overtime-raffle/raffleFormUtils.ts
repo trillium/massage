f9bdfd829e6948bd27e8215d0d7037bc32999634
Trillium Smith <trillium@trilliumsmith.com>
2026-06-15 15:10:05 -0700

    style(raffle): format multi-line export in raffleFormUtils

--- app/overtime-raffle/raffleFormUtils.ts
+++ app/overtime-raffle/raffleFormUtils.ts
@@ -1,2 +1,7 @@
 export type { RaffleFormProps, FormValues } from '../nerdstage-raffle/raffleFormUtils'
-export { validate, inputClasses, labelClasses, checkboxClasses } from '../nerdstage-raffle/raffleFormUtils'
+export {
+  validate,
+  inputClasses,
+  labelClasses,
+  checkboxClasses,
+} from '../nerdstage-raffle/raffleFormUtils'
